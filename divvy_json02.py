8a121de9ab4bbd79b85d7e1fa0181e64d3e23c9f
Neal <17957975+nealalan@users.noreply.github.com>
2019-11-15 18:36:23 -0600

    updt divvy_json02.py

--- divvy_json02.py
+++ divvy_json02.py
@@ -1,14 +1,66 @@
+###############################################################################
+# neal dreher 2017-12-09, updated 2019-11-15
+# https://github.com/nealalan/Transportation/blob/master/divvy_json02.py
+#
+# INFO: This python3 script will pull publically available JSON datasets containing
+#       realtime data for the Chicago DIVVY Bike Share Stations
+#
+# dependencies:
+# $ pip3 install requests thespian
+# use:
+# $ python3 divvy_json02.py 
+# 
+# DIVVY UPDATED THEIR DATASETS (between 2018 and 2019)
+# - station names are no longer available in a single dataset => add code 
+#   to read the stationName from the stationID
+# - the method of reporting TOTAL DOCKS vs AVAIL DOCKS no longer works
+#   the same => pull from CAPACITY in StationInfo file
+#
+###############################################################################
+# JSON DATA LAYOUT
+#
+# Station Status (realtime counts) -- 'https://gbfs.divvybikes.com/gbfs/en/station_status.json'
+#
+# {"last_updated":1573850008,"ttl":2,"data":
+#  {"stations":
+#   {"station_id":"331","num_bikes_available":7,"num_ebikes_available":0,"num_bikes_disabled":0,
+#    "num_docks_available":20,"num_docks_disabled":0,"is_installed":1,
+#    "is_renting":1,"is_returning":1,"last_reported":1573849499,"eightd_has_available_keys":false},
+# ]}}
+#
+# System information (station information) -- 'https://gbfs.divvybikes.com/gbfs/en/station_information.json'
+#
+# {"last_updated":1573852861,"ttl":2,"data":
+#  {"stations":[
+#   {"station_id":"331","external_id":"a3aacf29-a135-11e9-9cda-0a87ae2ba916",
+#    "name":"Halsted St & Clybourn Ave",
+#    "short_name":"331","lat":41.909668,"lon":-87.648128,"rental_methods":["CREDITCARD","TRANSITCARD","KEY"],
+#    "capacity":27,"electric_bike_surcharge_waiver":false,"eightd_has_key_dispenser":false,"has_kiosk":true}
+# ]}}
+#
+###############################################################################
+## Total docks = bike available + bikes unavailable + docks avail + docks unavail
+##   this is also the "capacity" field in Station Information dataset
+#
+# Note: There is no way to account for valet stations with bikes locked by cable vs to a dock
+###############################################################################
+
 import urllib.request
+import requests
 import json
-##grab the live json dataset
-url = 'https://www.divvybikes.com/stations/json'
-req = urllib.request.Request(url)
 
-##parse response into object
-r = urllib.request.urlopen(req).read()
-divvy = json.loads(r.decode('utf-8'))
+## LITERALS
+homeStation = ['331', '333']  # Halsted & Clybourn
+DATASETNAME = 'stations' # was 'stationBeanList'
+AVAILBIKES = 'num_bikes_available'
+AVAILDOCKS = 'num_docks_available'
+UNAVAILBIKES = 'num_bikes_disabled'
+UNAVAILDOCKS = 'num_docks_disabled'
+STATIONID = 'station_id'
+STATIONNAME = 'name'
+TOTALDOCKS = 'capacity'
 
-homeStation = 100
+## VARS
 counter = 0
 availableBikes = 0
 availableDocks = 0
@@ -18,42 +70,69 @@ badCnt = 0
 badTot = 0
 systemWideDocks = 0
 
-print("DIVVY BIKE DATA REPORTING")
-for item in divvy['stationBeanList']:
+## GRAB THE LIVE JSON DATASET FROM DIVVY
+#OLD url = 'https://www.divvybikes.com/stations/json'
+## request data and parse into object
+stationStatusURL = 'https://gbfs.divvybikes.com/gbfs/en/station_status.json'
+divvyStationStatusReponse = urllib.request.urlopen(urllib.request.Request(stationStatusURL)).read()
+divvyStationStatusDataset = json.loads(divvyStationStatusReponse.decode('utf-8'))
+stationInfoURL = 'https://gbfs.divvybikes.com/gbfs/en/station_information.json'
+divvyStationInfoResponse = urllib.request.urlopen(urllib.request.Request(stationInfoURL)).read()
+divvyStationInfoDataSet = json.loads(divvyStationInfoResponse.decode('utf-8'))
+
+## accums for the DATA SUMMARY section
+def counters(passed_item, passed_station):
+    global counter, availableBikes, availableDocks, systemWideDocks
     counter += 1
-    availableBikes += item['availableBikes']
-    availableDocks += item['availableDocks']
-    systemWideDocks += item['totalDocks']
-    if item['availableBikes'] == 0:
-        noBikes += 1
-        if noBikes == 1:
-            print("Stations with no available bikes:")
-        print(item['stationName'], " Bikes:", item['availableBikes'], " Docks:", item['availableDocks'], " TotalDocks:", item['totalDocks'])
+    availableBikes += passed_item[AVAILBIKES]
+    availableDocks += passed_item[AVAILDOCKS]
+    systemWideDocks += passed_item[AVAILBIKES] + passed_item[AVAILDOCKS] + passed_item[UNAVAILBIKES] + passed_item[UNAVAILDOCKS]
 
-for item in divvy['stationBeanList']:
-    if item['availableDocks'] == 0:
+    global badCnt, badTot, noDocks, noBikes
+    # count empty or full stations
+    if passed_item[AVAILDOCKS] == 0:
         noDocks += 1
-        if noDocks == 1:
-            print("\nStations with no available docks:")
-        print(item['stationName'], " Bikes:", item['availableBikes'], " Docks:", item['availableDocks'], " TotalDocks:", item['totalDocks'])
+    if passed_item[AVAILBIKES] == 0:
+        noBikes += 1
+    # account for bad station and equip totals
+    badTot += badStation
+    if badStation > 0:
+        badCnt = badCnt + 1
+        #print('Sta:', badStation, ' Tot:', badTot, ' Cnt:', badCnt)
 
-for item in divvy['stationBeanList']:
-    if item['availableBikes'] + item['availableDocks'] != item['totalDocks']:
-        badCnt += 1
-        if badCnt == 1:
-                print("\nStation with bad bike or dock (3+):")
-        badStation = item['availableBikes'] + item['availableDocks'] - item['totalDocks']
-        badTot = badTot + badStation
-        if abs(badStation) > 2:
-            print(item['stationName'], " Bikes:", item['availableBikes'], " Docks:", item['availableDocks'], " TotalDocks:", item['totalDocks'], " Errors:", abs(badStation))
+## format each output line item, 
+##  including a read of the text name from a different dataset
+def printers(passed_item, passed_station):
 
-print("\nDIVVY BIKE DATA SUMMARY")
-print("Total Stations:", counter, " Total Docks:", systemWideDocks)
-print("Avail Bikes:", availableBikes, " Avail Docks:", availableDocks)
-print("Stations with no bikes:", noBikes, " Stations with no docks:", noDocks)
-print("Stations w/ potential repairs:", badCnt, " Equipment w/ potential repairs:", abs(badTot))
+    # print the line
+    print('{:{width}}'.format(passed_item[STATIONID], width=3),
+        '{:36.36}'.format(passed_station[STATIONNAME]),
+        " Bikes:", '{0:{width}}'.format(passed_item[AVAILBIKES], width=2),
+        " Docks:", '{0:{width}}'.format(passed_item[AVAILDOCKS], width=2),
+        " TotalDocks:", '{0:{width}}'.format(
+            passed_station[TOTALDOCKS], width=3),
+            #passed_item[AVAILBIKES] + passed_item[AVAILDOCKS] + 
+            #passed_item[UNAVAILBIKES] + passed_item[UNAVAILDOCKS], width=3),
+        " Errors:", '{0:{width}}'.format(abs(badStation), width=2))
 
-for item in divvy['stationBeanList']:
-    if item['id'] == homeStation:
-        print('\nHOME STATION STATUS:', item['stationName'])
-        print("Bikes:", item['availableBikes'], " Docks:", item['availableDocks'], " TotalDocks:", item['totalDocks'])
+## RUN SCRIPT STARTING HERE
+print("DIVVY BIKE DATA REPORTING")
+
+## LIST ALL STATIONS, SHOW WITH ERRORS
+for item in divvyStationStatusDataset['data'][DATASETNAME]:
+    for station in divvyStationInfoDataSet['data'][DATASETNAME]:
+        if station[STATIONID] == item[STATIONID]:
+            badStation = station[TOTALDOCKS] - (item[AVAILBIKES] + item[AVAILDOCKS])
+            counters(item, station)
+            printers(item, station)
+
+## PRINT SUMMARY
+print("\nDIVVY BIKE DATA SUMMARY\n",
+      "     Total Stations:", '{0:{width}}'.format(counter, width=5),
+      "             Stations with no bikes:", '{0:{width}}'.format(noBikes, width=4), "\n",
+      "        Total Docks:", '{0:{width}}'.format(systemWideDocks, width=5),
+      "             Stations with no docks:", '{0:{width}}'.format(noDocks, width=4), "\n",
+      "        Avail Bikes:", '{0:{width}}'.format(availableBikes, width=5),
+      "      Stations w/ potential repairs:", '{0:{width}}'.format(badCnt, width=4), "\n",
+      "        Avail Docks:", '{0:{width}}'.format(availableDocks, width=5),
+      "     Equipment w/ potential repairs:", '{0:{width}}'.format(abs(badTot), width=4))
